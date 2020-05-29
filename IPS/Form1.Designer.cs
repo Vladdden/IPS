3094f779784d2b3e45f1880873303e5b02457017
Vladislav <Vlad2999009@yandex.ru>
2020-05-29 11:07:12 +0300

    Initial commit

---
+++ IPS/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace IPS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.comboBoxTables = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(254, 17);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(56, 21);
+            this.buttonOK.TabIndex = 7;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // comboBoxTables
+            // 
+            this.comboBoxTables.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxTables.FormattingEnabled = true;
+            this.comboBoxTables.Location = new System.Drawing.Point(83, 17);
+            this.comboBoxTables.Name = "comboBoxTables";
+            this.comboBoxTables.Size = new System.Drawing.Size(165, 21);
+            this.comboBoxTables.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 32);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Select table to show:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 49);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ShowCellToolTips = false;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 389);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.DataError += new System.Windows.Forms.DataGridViewDataErrorEventHandler(this.dataGridView1_DataError);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.comboBoxTables);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.ComboBox comboBoxTables;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ IPS/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+//using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlServerCe;
+using FirebirdSql.Data.FirebirdClient;
+
+namespace IPS
+{
+    public partial class Form1 : Form
+    {
+
+
+        private static string ServerName = @"localhost\sqlexpress";
+        private static string Port = "3306";
+        private static string Username = @"Владислав-ПК\\Владислав";
+        private static string Password = "";
+        private static string DBName = "IPSArchive";
+        //private static string connectionString = @"server=" + ServerName + ";port=" + Port + ";username=" + Username + ";password=" + Password + ";database=" + DBName + ";";
+        //private static string connectionString = @"Data Source=ВЛАДИСЛАВ-ПК\SQLEXPRESS;Initial Catalog=IPSArchive;Integrated Security=True";
+        //private string connectionString = "Data Source=MyData.sdf;Encrypt Database=True;Password=myPassword;File Mode=shared read;Persist Security Info=False;";
+        //private string connectionString = "Data Source = MyData.sdf; Max Database Size=256;Persist Security Info=False;";
+        //private string connectionString = "User=SYSDBA;Password=masterkey;Database=SampleDatabase.fdb;DataSource=localhost;Port=3050;Dialect=3;Charset=NONE;Role=;Connection lifetime = 15; Pooling=true;MinPoolSize=0;MaxPoolSize=50;Packet Size = 8192;ServerType=0;";
+        private FbConnection conn;
+        private FbCommand cmd;
+        SqlCeDataAdapter adapter;
+        DataTable dtMain;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //SqlCeEngine en = new SqlCeEngine(connectionString);
+            //en.CreateDatabase();
+            //conn = new SqlCeConnection(connectionString);
+
+            try
+            {
+                FbConnectionStringBuilder fb_con = new FbConnectionStringBuilder();
+                fb_con.Database = @"localhost:C:\Users\Владислав\source\repos\IPS\IPS\FBASE.FDB"; //путь к файлу базы данных
+                fb_con.ServerType = FbServerType.Embedded;//указываем тип сервера (0 - "полноценный Firebird" (classic или super server), 1 - встроенный (embedded))
+                fb_con.UserID = "SYSDBA";
+                fb_con.Password = "masterkey";
+                fb_con.ClientLibrary = "fbclient.dll";
+                //создаем подключение
+                using (conn = new FbConnection(fb_con.ToString())) //передаем нашу строку подключения объекту класса FbConnection
+                {
+                    conn.Open();
+                    FbTransaction myTransaction = conn.BeginTransaction();
+                    using (cmd = new FbCommand("select * from Users", conn, myTransaction))
+                    {
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                var values = new object[reader.FieldCount];
+                                reader.GetValues(values);
+                                Console.WriteLine(string.Join("|", values));
+                            }
+                        }
+                    }
+                    cmd.ExecuteNonQuery();
+                }
+                MessageBox.Show("ok");
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.ReadLine();
+
+            /* 0000
+            using (var command = new FbCommand("select * from demo", connection, transaction))
+            {
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var values = new object[reader.FieldCount];
+                        reader.GetValues(values);
+                        Console.WriteLine(string.Join("|", values));
+                    }
+                }
+            }
+            0000 */ 
+            // connect to server
+            // to database "master" to check if our database exists
+            // to create it if it isn't exists
+            //Console.WriteLine("Подключение");
+
+            /*{
+                Console.WriteLine("Подключение открыто");
+                Console.WriteLine("Свойства подключения:");
+                Console.WriteLine("\tСтрока подключения: {0}", conn.ConnectionString);
+                Console.WriteLine("\tБаза данных: {0}", conn.Database);
+                Console.WriteLine("\tСервер: {0}", conn.DataSource);
+                Console.WriteLine("\tВерсия сервера: {0}", conn.ServerVersion);
+                Console.WriteLine("\tСостояние: {0}", conn.State);
+                //Console.WriteLine("\tWorkstationld: {0}", conn.WorkstationId);
+            }*/
+            // create database if not exists
+
+            /*try
+            {
+                using (cmd = new FbCommand(cmd_start, conn, transaction))
+                {
+                    Console.WriteLine(1);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (SqlCeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                // закрываем подключение
+                conn.Close();
+                Console.WriteLine("Подключение закрыто...");
+            }
+            */
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////
+            /*try
+            {
+                using (cmd = new SqlCeCommand(String.Format("CREATE DATABASE [{0}] ON (" +
+                                                    "    NAME = {0}, " +
+                                                    "    FILENAME = '" + Application.StartupPath + "\\{0}.mdf'" +
+                                                    ");",
+                                                    DBName), conn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (SqlCeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                // закрываем подключение
+                conn.Close();
+                Console.WriteLine("Подключение закрыто...");
+            }
+
+            // connect to db
+            /////conn = new SqlCeConnection(connectionString);
+            conn.Open();
+
+            // create table "Table 1" if not exists
+            using (SqlCeCommand cmd = new SqlCeCommand(String.Format(
+                                      "IF NOT EXISTS (" +
+                                      "    SELECT [name] " +
+                                      "    FROM sys.tables " +
+                                      "    WHERE [name] = '{0}'" +
+                                      ") " +
+                                      "CREATE TABLE [{0}] (" +
+                                      "    id [INT] IDENTITY(1,1) PRIMARY KEY CLUSTERED, " +
+                                      "    [text column] [TEXT] NULL, " +
+                                      "    [int column] [INT] NULL " +
+                                      ")",
+                                      "Table 1"), conn))
+            {
+                cmd.ExecuteNonQuery();
+            }
+            
+            // get all tables from DB
+            using (DataTable dt = conn.GetSchema("Tables"))
+            {
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    if (dt.Rows[i].ItemArray[dt.Columns.IndexOf("TABLE_TYPE")].ToString() == "BASE TABLE")
+                    {
+                        comboBoxTables.Items.Add(dt.Rows[i].ItemArray[dt.Columns.IndexOf("TABLE_NAME")].ToString());
+                    }
+                }
+            }//dtMain.Columns["id"].ReadOnly = true; // deprecate id field edit to prevent exceptions*/
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            if (comboBoxTables.SelectedItem == null) return;
+            //adapter = new SqlCeDataAdapter("SELECT * FROM [" + comboBoxTables.SelectedItem.ToString() + "]", conn);
+            new SqlCeCommandBuilder(adapter);
+            dtMain = new DataTable();
+            adapter.Fill(dtMain);
+            dataGridView1.DataSource = dtMain;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (adapter == null) return;
+
+            adapter.Update(dtMain);
+        }
+
+        // show tooltip (not intrusive MessageBox) when user trying to input letters into INT column cell
+        private void dataGridView1_DataError(object sender, DataGridViewDataErrorEventArgs e)
+        {
+            if (dtMain.Columns[e.ColumnIndex].DataType == typeof(Int64) ||
+                dtMain.Columns[e.ColumnIndex].DataType == typeof(Int32) ||
+                dtMain.Columns[e.ColumnIndex].DataType == typeof(Int16))
+            {
+                Rectangle rectColumn;
+                rectColumn = dataGridView1.GetColumnDisplayRectangle(e.ColumnIndex, false);
+
+                Rectangle rectRow;
+                rectRow = dataGridView1.GetRowDisplayRectangle(e.RowIndex, false);
+
+                toolTip1.ToolTipTitle = "This field is for numbers only.";
+                toolTip1.Show(" ",
+                          dataGridView1,
+                          rectColumn.Left, rectRow.Top + rectRow.Height);
+            }
+        }
+
+        private string cmd_start = @"
+
+        ";
+    }
+}
